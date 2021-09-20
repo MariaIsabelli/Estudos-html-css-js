0b309d539626795d7596028dfd895b665df87248
Maria Isabelli <89652212+MariaIsabelli@users.noreply.github.com>
2021-09-20 11:58:37 -0300

    Add files via upload

---
+++ terrorflix/app.js
@@ -0,0 +1,19 @@
+function adicionarFilme() {
+  var filmeFavorito = document.getElementById("filme").value;
+  if (filmeFavorito.endsWith(".jpg")) {
+    listarFilmesNaTela(filmeFavorito);
+  } else {
+    console.error("Endereço de filme inválido");
+  }
+  document.getElementById("filme").value = "";
+}
+
+function listarFilmesNaTela(filme) {
+  console.log(filme);
+  var elementoFilmeFavorito = "<img src=" + filme + ">";
+  var elementoListaFilmes = document.getElementById("listaFilmes");
+  elementoListaFilmes.innerHTML =
+    elementoListaFilmes.innerHTML + elementoFilmeFavorito;
+}
+
+  
