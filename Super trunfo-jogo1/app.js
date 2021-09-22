93634dee96ae702878250a3936e399a9bb3f2d4c
Maria Isabelli <89652212+MariaIsabelli@users.noreply.github.com>
2021-09-22 10:06:44 -0300

    Add files via upload

---
+++ Super trunfo-jogo1/app.js
@@ -0,0 +1,88 @@
+var carta1 = {
+    nome: "Bulbassauro",
+    atributos: {
+      ataque: 7,
+      defesa: 8,
+      magia: 6
+    }
+  };
+  
+  var carta2 = {
+    nome: "Pikachu",
+    atributos: {
+      ataque: 8,
+      defesa: 8,
+      magia: 9
+    }
+  };
+  
+  var carta3 = {
+    nome: "jigglypuff",
+    atributos: {
+      ataque: 9,
+      defesa: 6,
+      magia: 10
+    }
+  };
+  
+  var cartas = [carta1, carta2, carta3];
+  var cartaMaquina;
+  var cartaJogador;
+  
+  function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * 3);
+    cartaMaquina = cartas[numeroCartaMaquina];
+  
+    var numeroCartaJogador = parseInt(Math.random() * 3);
+    while (numeroCartaMaquina == numeroCartaJogador) {
+      numeroCartaJogador = parseInt(Math.random() * 3);
+    }
+    cartaJogador = cartas[numeroCartaJogador];
+    console.log(cartaJogador);
+  
+    document.getElementById("btnSortear").disabled = true;
+    document.getElementById("btnJogar").disabled = false;
+  
+    exibirOpcoes();
+  }
+  
+  function exibirOpcoes() {
+    var opcoes = document.getElementById("opcoes");
+    var opcoesTexto = "";
+  
+    for (var atributo in cartaJogador.atributos) {
+      opcoesTexto +=
+        "<input type='radio' name='atributo' value='" +
+        atributo +
+        "'>" +
+        atributo;
+    }
+    opcoes.innerHTML = opcoesTexto;
+  }
+  
+  function obtemAtributoSelecionado() {
+    var radioAtributos = document.getElementsByName("atributo");
+  
+    for (var i = 0; i < radioAtributos.length; i++) {
+      if (radioAtributos[i].checked == true) {
+        return radioAtributos[i].value;
+      }
+    }
+  }
+  
+  function jogar() {
+    var atributoSelecionado = obtemAtributoSelecionado();
+    var elementoResultado = document.getElementById("resultado");
+    var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+    var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+  
+    if (valorCartaJogador > valorCartaMaquina) {
+      elementoResultado.innerHTML = "Você venceu";
+    } else if (valorCartaMaquina > valorCartaJogador) {
+      elementoResultado.innerHTML = "Você perdeu, a carta da máquina é maior";
+    } else {
+      elementoResultado.innerHTML = "Empatou";
+    }
+    console.log(cartaMaquina);
+  }
+  
