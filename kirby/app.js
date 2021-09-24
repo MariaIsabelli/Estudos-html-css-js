2222e564f757fbc0ca3ffc096204884e1bbe6d46
Maria Isabelli <89652212+MariaIsabelli@users.noreply.github.com>
2021-09-24 12:05:25 -0300

    Add files via upload

---
+++ kirby/app.js
@@ -0,0 +1,20 @@
+/Javascript is imported from my other pen, 
+//otherwise it would blow up:
+//https://codepen.io/gregh/pen/Zexbwo
+
+//I created a project, where you can check the code in separate files:
+//https://codepen.io/gregh/project/editor/aAexRX/
+
+
+
+
+/*
+The video snippets are taken from the following YouTube videos:
+
+zelda: https://www.youtube.com/watch?v=hhb3LqLtTBM
+pacman: https://www.youtube.com/watch?v=jINUCRdN6lw
+mario: https://www.youtube.com/watch?v=rc62hFksKo4&
+metroid: https://www.youtube.com/watch?v=Ti1V0YMULGs
+kirby: https://www.youtube.com/watch?v=rJXM4EPbPe0
+
+*/
