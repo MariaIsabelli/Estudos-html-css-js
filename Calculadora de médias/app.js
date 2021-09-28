bd5f9240439943f19ffc2bf30963719e6baa27bf
Maria Isabelli <89652212+MariaIsabelli@users.noreply.github.com>
2021-09-28 11:45:35 -0300

    Add files via upload

---
+++ Calculadora de médias/app.js
@@ -0,0 +1,19 @@
+var  nome  =  "Maria Isabelli Pinto"
+
+var  NotaDoPrimeiroBimestre  =  9
+var  NotaDoSegundoBimestre  =  10
+var  NotaDoTerceiroBimestre  =  8
+var  NotaDoQuartoBimestre  =  6
+
+var  NotaFinal  =  ( NotaDoPrimeiroBimestre  +  NotaDoSegundoBimestre  +  NotaDoTerceiroBimestre  +  NotaDoQuartoBimestre )  /  4 
+
+
+var  NotaFixada  =  NotaFinal . toFixed ( 1 )
+
+console . log ( "Bem Vindo"  +  nome )
+console . log ( NotaFinal )
+
+if ( Se  NotaFinal  > =  7 )
+console . log ( "Voce passou !!! Parabens" )
+outro
+console . log ( "Sinto muito, no entanto voce reprovou" )
