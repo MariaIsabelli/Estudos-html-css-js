30f2da6da4255c2cd2ea5d83957699526004be7e
Maria Isabelli <89652212+MariaIsabelli@users.noreply.github.com>
2021-11-19 17:30:57 -0300

    portfolio
    criando portifolio

---
+++ personal-portfolio/dist/script.js
@@ -0,0 +1,172 @@
+$(document).ready(function() {
+
+  // import view port library function
+  jQuery.extend(verge);
+
+  // smooth scrolling of viewport to target
+  $('a[href^="#"]').click(function(event) {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top - 50
+    }, 900);
+  });
+
+  $('#down').click(function() {
+    $('html,body').animate({
+      scrollTop: $('#about').offset().top - 50
+    }, 900);
+  });
+
+  var currentTime = new Date();
+  var year = currentTime.getFullYear();
+
+  // data for the portfolio cards front and back
+  var projectData = [{
+      title: 'Quotes',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-random-quote-machine" target="_blank"><i>Build a Random Quote Machine</i></a>. The user is able to show a new random quote and send that quote to Twitter. <strong>Quotes</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br> <strong>Graphics Editor:</strong> Gimp, pixlr.com<br> <strong>Images:</strong> lorempixel.com<br> <strong>Fonts:</strong> Font-awesome<br> <strong>Data:</strong> random famous quotes API',
+
+      image: ['http://2am.ninja/img/quotes.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/full/dojjre'],
+        ['Live Site', 'fa-link', 'http://2am.ninja/quotes/']
+      ]
+    }, {
+      title: 'Twitch TV',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-use-the-twitchtv-json-api" target="_blank"><i>Use the Twitchtv JSON API</i></a>. A user can see if Free Code Camp is currently streaming on Twitch.tv.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br> <strong>Fonts:</strong> Ubuntu Mono<br> <strong>Data:</strong> Twitchtv API',
+
+      image: ['http://2am.ninja/img/twitch3.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/full/jrEBgq']
+  //      ,['Live Site', 'fa-link', 'http://2am.ninja/twitch/']
+      ]
+    }, {
+      title: 'Wiki Search',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-wikipedia-viewer" target="_blank"><i>Build a Wikipedia Viewer</i></a>. A user can search Wikipedia entries in a search box and see the resulting Wikipedia entries.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br> <strong>Graphics Editor:</strong> pixlr.com<br> <strong>Images:</strong> unsplash.com<br> <strong>Data:</strong> Wikipedia API',
+
+      image: ['http://2am.ninja/img/wiki.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/full/rVRwor']
+      ]
+    }, {
+      title: 'Pomodoro Timer',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-build-a-pomodoro-clock" target="_blank"><i>Build a Pomodoro Clock</i></a>. A user can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed. <strong>Pomodoro Timer</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, HTML5 Canvas, CSS3, Javascript, jQuery, ion.sound<br> <strong>Layout:</strong> Bootstrap, HTML5 Canvas<br> <strong>Fonts:</strong> IcoMoon',
+
+      image: ['http://2am.ninja/img/timer.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/full/WvVZyM'],
+        ['Live Site', 'fa-link', 'http://2am.ninja/timer/']
+      ]
+    }, {
+      title: 'Local Weather',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-show-the-local-weather" target="_blank"><i>Show the Local Weather</i></a>. A user can see the weather in my current location.. <strong>Local Weather</strong> was designed and optimized to be a mobile web app first.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br> <strong>Images:</strong> flickr.com<br> <strong>Fonts:</strong> Climacons, Open Sans<br> <strong>Data:</strong> Open Weather Map API, IP-API.com Geolocation API',
+
+      image: ['http://2am.ninja/img/weather.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://codepen.io/MutantSpore/full/oXaoxb/']
+      ]
+    },
+    /*                    
+                        {
+         title: 'Camper News',
+         text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/zipline-stylize-stories-on-camper-news" target="_blank"><i>Stylize Stories on Camper News</i></a>. The user can browse recent posts from Camper News.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap, Masonary<br> <strong>Graphics Editor:</strong> Gimp, pixlr.com<br> <strong>Images:</strong> unsplash.com<br>  <strong>Data:</strong> Free Code Camp news API',
+
+         image: ['http://2am.ninja/img/news.jpg'],
+         site: [
+           ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/debug/vOvBqY']
+         ]
+       }, 
+     */
+    {
+      title: 'Bar Graph',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/visualize-data-with-a-bar-chart" target="_blank"><i>Visualize Data with a Bar Chart </i></a>. The user can see US Gross Domestic Product by quarter, over time, with a mouse over tooltip.</p> <strong>Code:</strong> HTML5, CSS3, D3, SVG, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap',
+
+      image: ['http://2am.ninja/img/bar-graph.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/full/PNJMpg']
+      ]
+    }, {
+      title: 'Heat Map',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/visualize-data-with-a-heat-map" target="_blank"><i>Visualize Data with a Heat Map</i></a>. The user can view a heat map with data represented both on the Y and X axis, with mouse over tooltip.</p> <strong>Code:</strong> HTML5, CSS3, D3, SVG, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap',
+
+      image: ['http://2am.ninja/img/heat-map.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/full/VeKNEa']
+      ]
+    },
+    /*                 
+                     {
+      title: 'Force Graph',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/show-relationships-with-a-force-directed-graph" target="_blank"><i>Show Relationships with a Force Directed Graph</i></a>. The user can see a Force-directed Graph that shows which campers are posting links on Camper News to which domains.</p> <strong>Code:</strong> HTML5, CSS3, D3, SVG, Javascript, jQuery<br> <strong>Layout:</strong> Bootstrap<br>  <strong>Data:</strong> Free Code Camp news API',
+
+      image: ['http://2am.ninja/img/force-graph.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/debug/KVdbMO']
+      ]
+    }, 
+    
+    */
+    {
+      title: 'Tic-Tac-Toe',
+      text: '<p>A FreeCodeCamp project, <a href="https://www.freecodecamp.com/challenges/build-a-tic-tac-toe-game" target="_blank"><i>Build a Tic Tac Toe Game</i></a>. The user can play a game of Tic Tac Toe with the computer.</p> <strong>Code:</strong> HTML5, CSS3, Javascript, jQuery<br> <strong>Layout:</strong> Materialize.css',
+
+      image: ['http://2am.ninja/img/ttt.jpg'],
+      site: [
+        ['CodePen', 'fa-codepen', 'https://s.codepen.io/MutantSpore/full/jWWYLo']
+      ]
+    }
+  ];
+
+  // add listener to all the cards for click flipping
+  function addListener() {
+    var cards = document.querySelectorAll(".card.effect_click");
+
+    for (var i = 0; i < cards.length; i++) {
+      clickListener(cards[i]);
+    }
+
+    function clickListener(card) {
+      card.addEventListener("click", function() {
+        this.classList.toggle("flipped");
+      });
+    }
+  }
+
+  // create, populate and show portfolio project cards
+  function showProjectCards() {
+    var html = '';
+
+    projectData.forEach(function(project) {
+      html += '<div class="col-sm-6 col-md-4">';
+      html += '<div class="card effect_click"><div class="card_front">';
+      html += '<figure><img class="img-responsive" src="' + project.image[0] + '">';
+      html += '<figcaption class="project-title">';
+      html += project.title;
+      html += '</figcaption></figure></div>';
+
+      html += '<div class="card_back"><figure>';
+      html += '<div class="project-title">' + project.title + '</div>';
+      html += '<figcaption">';
+      html += '<div class="project-body">' + project.text + '</div>';
+      html += '<div><a data-toggle="tooltip" title="' + project.site[0][0] + '" data-placement="top" href="' + project.site[0][2] + '" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk0"><i class="fa ' + project.site[0][1] + '" aria-hidden="true"></i></a>';
+
+      if (typeof project.site[1] !== 'undefined') {
+        html += '<a data-toggle="tooltip" title="' + project.site[1][0] + '" data-placement="top" href="' + project.site[1][2] + '" target="_blank" class="btn btn-primary btn-lg btn-circle btn-lnk btn-lnk1"><i class="fa ' + project.site[1][1] + '" aria-hidden="true"></i></a>';
+      }
+
+      html += '</div></figcaption></figure></div>';
+      html += '</div>';
+      html += '</div>';
+    });
+
+    $('#theProjects').append(html);
+    addListener();
+  }
+
+  showProjectCards();
+  $(".copyright").append("<span>© " + year + " Bruce Young. All rights reserved</span>");
+
+  // turn on bootstrap tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  
+});
